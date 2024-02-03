023f66d46f78ccd51298f336d2948176a30da05f
Hideko <hideko332200@gmail.com>
2024-02-04 02:39:41 +0900

    [change] adapter cloudflare -> vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import adapter from "@sveltejs/adapter-cloudflare";
+import adapter from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
