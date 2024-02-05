f7f1e47be75bf4e59fa0bd4a027393aa51a97858
Hideko <hideko332200@gmail.com>
2024-02-05 22:45:22 +0900

    ee

--- src/lib/cms.ts
+++ src/lib/cms.ts
@@ -4,6 +4,6 @@ const cms_domain = import.meta.env.VITE_MICROCMS_SERVICE_DOMAIN
 const cms_key = import.meta.env.VITE_MICROCMS_API_KEY
 
 export const cms = createClient({
-    serviceDomain: cms_domain,
+    serviceDomain: "hideko-portfolio.microcms.io",
     apiKey: cms_key
 });
