73345ec0628822e5794b9f49361a67a7687fa9a8
Hideko <hideko332200@gmail.com>
2024-02-06 00:44:52 +0900

    [chang] adapter netlify -> cf

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import adapter from "@sveltejs/adapter-netlify";
+import adapter from "@sveltejs/adapter-cloudflare";
 
 // extended.mjs
 /** @type {import('@sveltejs/kit').Config} */
