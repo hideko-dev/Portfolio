9e8683f2f8fc766f5bb4c137dabb4f1f273eafe7
Hideko <hideko332200@gmail.com>
2024-02-07 23:46:40 +0900

    e

--- src/routes/meta.svelte
+++ src/routes/meta.svelte
@@ -2,6 +2,7 @@
     <meta name="description" content="This is Hideko's portfolio site.">
     <meta property="og:title" content="Hideko">
     <meta property="og:description" content="This is Hideko's portfolio site.">
-    <meta property="og:image" content="https://cdn.hideko-dev.com/1707060172952.png">
+    <meta property="og:image" content="https://cdn.hideko-dev.com/favicon.png">
     <meta property="og:url" content="https://hideko-dev.com">
+    <meta name="twitter:card" content="https://cdn.hideko-dev.com/shapes-dark%20(1).png">
 </svelte:head>
